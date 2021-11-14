2df6ac788c07a55c1bfc602d983dfbc6ea5152c9
fcaimi <federico.caimi@mercadolibre.com>
2021-11-14 18:46:19 -0300

    se quita la dependencia a un controller que no existe

--- Catalog/controllers/controller.js
+++ Catalog/controllers/controller.js
@@ -2,11 +2,9 @@ const router = require('express').Router()
 
 const sensorController = require('./sensorController')
 const propertiesController = require('./propertiesController')
-const loginController = require('./loginController')
 
 router.use('/sensor',sensorController)
 router.use('/property',propertiesController)
-router.use('/login',loginController)
 
 module.exports = router
 
