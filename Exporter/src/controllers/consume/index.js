3aa6d665734834198d9213324d47c36ff5a78472
DiegoBaccino1 <d.baccino@outlook.com>
2021-11-21 14:11:57 -0300

    Fix varios para consumir data de observations (#22)
    * Fix varios para consumir data de observations
    
    * Obtencion de timestamp del ultimo registro de observations

--- Exporter/src/controllers/consume/index.js
+++ Exporter/src/controllers/consume/index.js
@@ -2,12 +2,10 @@ const {getData}=require('../../services/consume')
 const consume = async (req,res)=>{
     try{
         var response = await getData(req.params.ID)
-        if (response.status == 200){
-            return response.data
-        }
+        res.send(response)
     }catch(err){
         console.log(err)
-        return null
+        res.status(500).send(err.message)
     }
     
 }
--- Observations/repositories/mongoRepository.js
+++ Observations/repositories/mongoRepository.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/test', {
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 3600,
+    serverSelectionTimeoutMS: 36000,
 });
 
 const properties = new mongoose.Schema({
