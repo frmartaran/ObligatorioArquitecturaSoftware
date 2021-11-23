7e3b7362649923ec01de11cab31915eec836f9c4
fcaimi <federico.caimi@mercadolibre.com>
2021-11-22 23:22:23 -0300

    se genera script de sensor que realiza request contra gateway

--- SensorBot/index.js
+++ SensorBot/index.js
@@ -1,8 +1,61 @@
 const sender = require('./sender')
+var fs = require('fs');
+
 
 async function recurse() {
 
   while(true){
+    var configFile = JSON.parse(fs.readFileSync('./config/default.json', 'utf8'));
+    var sensor = configFile.sensor
+
+    var json = createRequestJson(sensor)
+      
+    console.log(json)
+    let resp = sender.sender(json)
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+    await delay(configFile.latencia)
+  }
+}
+
+recurse()
+
+function randomIntFromInterval(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function createRequestJson(sensor) { 
+  
+  let requestProperties = createrequestProperties(sensor)
+  var json = {
+    "ESN":sensor.ESN,
+    "name":sensor.name,
+    "location": sensor.location,
+    "date":1636938049,
+    "properties": requestProperties
+  }
+  return json
+}
+
+function createrequestProperties(sensor){
+  let requestProperties = []
+
+  sensor.properties.forEach( prop =>{
+    const value = randomIntFromInterval(prop.minUnitValue, prop.maxUnitValue)
+    let newProp = {
+      "Name": prop.name,
+      "Value": value,
+      "unit": prop.unit
+    }  
+    requestProperties.push(newProp)
+  })
+  return requestProperties
+}
+
+
+
+
+/*
+while(true){
     var i = Math.round(Math.random()*10) 
     var json = {
       "ESN":"S"+i,
@@ -21,5 +74,4 @@ async function recurse() {
     const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
     await delay(500)
   }
-}
-recurse()
+*/
