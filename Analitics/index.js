44d7d752ee8afe1071f5bff5eeefcc5e56a04ca9
Diego Baccino <d.baccino@outlook.com>
2021-11-24 01:12:05 -0300

    Se agrega middleware para manejo de errores en api requests

--- Analitics/index.js
+++ Analitics/index.js
@@ -1,23 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const Queue = require('bull')
-
+const {handleApiError} = require('../ErrorHandler/api_errors')
 const app = express()
 const router = require('./routes')
 
 app.use('/analytics',router)
 app.use(express.json())
+app.use(handleApiError)
+
 const port = process.env.PORT 
 const validationDataQueue = new Queue(process.env.QUEUE_NAME);
 
 app.listen(port,()=>{
     console.log(`Server start listening on http://localhost:${port}`)
 })
-
-app.post('/test',(req,res)=>{
-    validationDataQueue.add(req.body)
-    res.send()
-})
-
 
 validationDataQueue.process(8,__dirname+'/services/processes/validate/index.js')
